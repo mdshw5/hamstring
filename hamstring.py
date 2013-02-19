c8e19ba03180f33d43ecbbaeced5c58e63fe2f2f
Matt Shirley <mdshw5@gmail.com>
2013-02-19 14:02:49 -0500

    GPL disclaimer

--- hamstring.py
+++ hamstring.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
+#
+# Copyright (C) 2013 Matt Shirley
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 # Principles used in this script are adapted from the 2012 publication:
 # Bystrykh, L. V. (2012). Generalized DNA Barcode Design Based on Hamming Codes. 
 # PLoS ONE, 7(5), e36852. doi:10.1371/journal.pone.0036852.t004
-## To Do
+#
+#
+# To Do
 #- Implement binary to quaternary conversion (Done)
 #- Implement quaternary Hamming encoding function (Done)
 #- Implement quaternary Hamming decoding and checksumming function (Done)
