32b3bdf48f49e14a7dd14fee3604c10be6aafb75
Matt Shirley <mdshw5@gmail.com>
2013-03-30 21:58:55 -0400

    Added dist-utils setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='hamstring',
+      version='1.0',
+      author='Matt Shirley',
+      author_email='mdshw5@gmail.com',
+      url='https://github.com/mdshw5/hamstring',
+      download_url='git://github.com/mdshw5/hamstring.git',
+      py_modules=['hamstring'],
+      )
